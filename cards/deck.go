f26d3f05a01fb4b366d0f550cbb30163fa28fb99
Ckai <ckai.trans@gmail.com>
2023-04-21 11:27:14 +0800

    U21 Creating a New Deck

--- cards/deck.go
+++ cards/deck.go
@@ -7,11 +7,16 @@ type deck []string
 
 func newDeck() deck {
 	cards := deck{}
-
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
 	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
-	// 用巢狀迴圈將各種花色與數值組成的牌張加入 cards
+	// 用巢狀迴圈將各種花色與數值組成的牌張加入 cards。由於本邏輯不需使用索引值，因此用 _ 取代 i, j 兩個變數。
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
 }
 
 func (d deck) print() {
